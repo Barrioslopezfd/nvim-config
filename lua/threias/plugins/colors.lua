a072f552da735a604cda4bfa236552d722e2a8b3
Barrioslopezfd <barrioslopezfd@gmail.com>
2024-08-24 16:01:50 -0300

    i dont like the tokyo bg

--- lua/threias/plugins/colors.lua
+++ lua/threias/plugins/colors.lua
@@ -10,7 +10,20 @@ return {
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      require('tokyonight').setup {
+        style = 'night',
+        transparent = true,
+        terminal_colors = true,
+        styles = {
+          comments = { italic = false },
+          keyword = { italic = false },
+          -- Background styles. Can be "dark", "transparent" or "normal"
+          sidebars = 'dark', -- style for sidebars, see below
+          floats = 'dark', -- style for floating windows
+        },
+      }
+
+      vim.cmd.colorscheme 'tokyonight'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
