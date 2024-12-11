3526b3673da52a60e02a6b7f0200b0c7cfa99e5f
Barrioslopezfd <barrioslopezfd@gmail.com>
2024-12-10 22:43:04 -0300

    Arrowkeys bind to Nop / Add: lil keymap for go err

--- lua/threias/remap.lua
+++ lua/threias/remap.lua
@@ -48,3 +48,8 @@ vim.keymap.set('n', '<left>', '<Nop>')
 vim.keymap.set('n', '<right>', '<Nop>')
 vim.keymap.set('n', '<up>', '<Nop>')
 vim.keymap.set('n', '<down>', '<Nop>')
+vim.keymap.set(
+    "n",
+    "<leader>ee",
+    "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
+)
